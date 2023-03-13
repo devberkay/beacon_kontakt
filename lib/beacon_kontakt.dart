f0f87790daa2a56ce104d304402549480233891e
devberkay <canbr29@gmail.com>
2023-03-13 14:01:02 +0000

    Mar 13, 2023, 4:01 PM

--- lib/beacon_kontakt.dart
+++ lib/beacon_kontakt.dart
@@ -1,8 +1,12 @@
 
+import 'package:flutter/services.dart';
+
 import 'beacon_kontakt_platform_interface.dart';
 
 class BeaconKontakt {
+  static const MethodChannel _channel = const MethodChannel('beacon_kontakt');
   Future<String?> getPlatformVersion() {
     return BeaconKontaktPlatform.instance.getPlatformVersion();
   }
+  
 }
