3b96ed33554b87a7bd5e48a0c2bc9b52e7aac086
devberkay <canbr29@gmail.com>
2023-03-13 13:09:16 +0000

    Mar 13, 2023, 3:09 PM

--- lib/beacon_kontakt_method_channel.dart
+++ lib/beacon_kontakt_method_channel.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-
 import 'beacon_kontakt_platform_interface.dart';
-
 /// An implementation of [BeaconKontaktPlatform] that uses method channels.
 class MethodChannelBeaconKontakt extends BeaconKontaktPlatform {
   /// The method channel used to interact with the native platform.
