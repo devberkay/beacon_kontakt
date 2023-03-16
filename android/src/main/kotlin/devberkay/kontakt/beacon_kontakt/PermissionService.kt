8abc3b54824dffa8b16cd84dc667422133eb6332
devberkay <canbr29@gmail.com>
2023-03-16 12:50:02 +0000

    Mar 16, 2023, 2:50 PM

--- android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/PermissionService.kt
+++ android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/PermissionService.kt
@@ -14,7 +14,7 @@ class PermissionService(private val activity: Activity,private val context: Cont
 
      fun checkPermissions() {
         val requiredPermissions = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION)
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION) // some android 12 devices requires coarse_location with fine_location
         } else {
             arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
         }
