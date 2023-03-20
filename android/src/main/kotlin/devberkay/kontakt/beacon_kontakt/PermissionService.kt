458e6c90cc67ad7072a53be977f5687f50201f69
devberkay <canbr29@gmail.com>
2023-03-20 20:13:29 +0000

    Mar 20, 2023, 10:13 PM

--- android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/PermissionService.kt
+++ android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/PermissionService.kt
@@ -12,7 +12,7 @@ import io.flutter.plugin.common.EventChannel
 class PermissionService(private val activity: Activity,private val context: Context) {
 
 
-    fun checkPermissions() {
+    fun checkPermissions() : boolean {
         val requiredPermissions = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION) // some android 12 devices requires coarse_location with fine_location
         } else {
@@ -21,9 +21,10 @@ class PermissionService(private val activity: Activity,private val context: Cont
 
         if (isAnyOfPermissionsNotGranted(requiredPermissions)) {
             ActivityCompat.requestPermissions(activity, requiredPermissions, REQUEST_CODE_PERMISSIONS)
+            return false
         }
         else {
-
+            return true
         }
     }
 
