44284e330bf711474587697047a1c83474c91d4c
devberkay <canbr29@gmail.com>
2023-03-20 21:16:00 +0000

    Mar 20, 2023, 11:16 PM

--- android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
+++ android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
@@ -103,7 +103,7 @@ class ForegroundScanService(private val context: Context, private val apiKey : S
     fun stopScanning() {
         if (proximityManager.isScanning) {
             proximityManager.stopScanning()
-            Log.i(TAG, "Scanning stopped")
+
         }
     }
 
