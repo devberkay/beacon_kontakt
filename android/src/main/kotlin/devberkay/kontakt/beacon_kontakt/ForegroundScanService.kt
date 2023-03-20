8b7f61fdf7db71a22a4ab06a6a61ab87893da480
devberkay <canbr29@gmail.com>
2023-03-20 06:10:07 +0000

    Mar 20, 2023, 8:10 AM

--- android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
+++ android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
@@ -58,7 +58,7 @@ class ForegroundScanService(private val context: Context) : EventChannel.StreamH
             iBeacons: MutableList<IBeaconDevice>?,
             region: IBeaconRegion?
         ) {
-            eventSink?.success(iBeacons?.map { mapOf("rssi" to it.rssi, "txPower" to it.txPower , "batteryLevel" to it.batteryPower, "name" to it.name, "minor" to it.minor, "major" to it.major, "proximityUUID" to it.proximityUUID, "proximity" to it.proximity )  })
+            eventSink?.success(iBeacons?.map { mapOf("rssi" to it.rssi, "txPower" to it.txPower , "batteryLevel" to it.batteryPower, "name" to it.name, "minor" to it.minor, "major" to it.major, "proximityUUID" to it.proximityUUID,"uniqueUUID" to it.uniqueId ,"proximity" to it.proximity )  })
         }
 
         override fun onIBeaconLost(iBeacon: IBeaconDevice?, region: IBeaconRegion?) {
