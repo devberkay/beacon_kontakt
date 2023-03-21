86fa88b6d93b6c7be4ee15a2cb434b35f8de8dd7
devberkay <canbr29@gmail.com>
2023-03-21 22:14:48 +0000

    Mar 22, 2023, 12:14 AM

--- android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
+++ android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
@@ -56,7 +56,12 @@ class ForegroundScanService(private val context: Context, private val apiKey : S
                 .kontaktScanFilters(KontaktScanFilter.DEFAULT_FILTERS_LIST)
 
 
+            when (listenerType) {
+                "iBeaconListener" -> this.setIBeaconListener(iBeaconListener)
+                "secureProfileListener" -> this.setSecureProfileListener(secureProfileListener)
+            }
 
+            proximityManager.spaces().iBeaconRegions(listOf(primaryRegion));
 
 
         }
@@ -130,12 +135,6 @@ class ForegroundScanService(private val context: Context, private val apiKey : S
 //                    proximityManager.setSecureProfileListener(secureProfileListener)
 //                    Log.d(TAG, "WAS NOT startScanning: SETTING IBEACON LISTENER")
 //                }
-                when (listenerType) {
-                    "iBeaconListener" -> proximityManager.setIBeaconListener(iBeaconListener)
-                    "secureProfileListener" -> proximityManager.setSecureProfileListener(secureProfileListener)
-                }
-
-                proximityManager.spaces().iBeaconRegions(listOf(primaryRegion));
                 proximityManager.startScanning()
             }
         }
