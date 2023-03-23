67c2f0d95840c9f43181d50c012d77fb901f80c9
devberkay <canbr29@gmail.com>
2023-03-23 03:17:09 +0000

    Mar 23, 2023, 5:17 AM

--- android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
+++ android/src/main/kotlin/devberkay/kontakt/beacon_kontakt/ForegroundScanService.kt
@@ -21,7 +21,7 @@ import java.util.concurrent.TimeUnit
 
 class ForegroundScanService(private val context: Context, private val apiKey : String, private val scanPeriod: ScanPeriod, private val listenerType: String,private val proximityUUID:String,private val major:Int?, private val minor:Int? ) : EventChannel.StreamHandler  {
     private var eventSink: EventChannel.EventSink? = null
-    private  var proximityManager: ProximityManager = ProximityManagerFactory.create(context, KontaktCloudFactory.create(apiKey)).apply {
+    private var proximityManager: ProximityManager = ProximityManagerFactory.create(context, KontaktCloudFactory.create(apiKey)).apply {
         configuration()
             .scanMode(ScanMode.BALANCED)
             .scanPeriod(scanPeriod)
@@ -34,8 +34,16 @@ class ForegroundScanService(private val context: Context, private val apiKey : S
             .monitoringEnabled(true)
             .monitoringSyncInterval(10)
             .kontaktScanFilters(KontaktScanFilter.DEFAULT_FILTERS_LIST)
-
+            .apply {
+                Log.d(TAG, "proximityManager filterList: $filterList")
+                when (listenerType) {
+                    "iBeaconListener" -> setIBeaconListener(iBeaconListener)
+                    "secureProfileListener" -> setSecureProfileListener(secureProfileListener)
+                }
+                spaces().iBeaconRegions(listOf(primaryRegion))
+            }
     }
+
 
     private val iBeaconListener = object : IBeaconListener {
         override fun onIBeaconDiscovered(iBeacon: IBeaconDevice?, region: IBeaconRegion?) {
@@ -100,21 +108,6 @@ class ForegroundScanService(private val context: Context, private val apiKey : S
         proximityManager.disconnect()
     }
 
-
-
-
-    init {
-
-        Log.d(TAG, "proximityManager filterList: $filterList")
-
-        when (listenerType) {
-            "iBeaconListener" -> proximityManager.setIBeaconListener(iBeaconListener)
-            "secureProfileListener" -> proximityManager.setSecureProfileListener(secureProfileListener)
-        }
-
-        proximityManager.spaces().iBeaconRegions(listOf(primaryRegion));
-
-    }
 
 
 
