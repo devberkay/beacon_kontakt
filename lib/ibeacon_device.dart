216ab39fa8b7ff06c86e710ac31f0b3d13e83708
devberkay <canbr29@gmail.com>
2023-06-14 21:55:22 +0000

    Jun 15, 2023, 12:55 AM

--- lib/ibeacon_device.dart
+++ lib/ibeacon_device.dart
@@ -4,6 +4,6 @@ class IBeaconDevice {
   final String proximityUUID;
   final String major;
   final String minor;
-  final int? rssi;
-  final int? txPower;
+  final int? rssi; // doesn't received on monitoring
+  final int? txPower; // doesn't received on monitoring
 }
