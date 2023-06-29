8dc0ce5f53a69656c29a569f2c42d902b2a66a35
devberkay <canbr29@gmail.com>
2023-06-29 18:04:16 +0000

    Jun 29, 2023, 9:04 PM

--- ios/Classes/SecureScanService.swift
+++ ios/Classes/SecureScanService.swift
@@ -1,37 +1,37 @@
+////
+////  SecureScanService.swift
+////  beacon_kontakt
+////
+////  Created by Berkay Can on 6/14/23.
+////
 //
-//  SecureScanService.swift
-//  beacon_kontakt
+//import Foundation
+//import Flutter
+//import KontaktSDK
 //
-//  Created by Berkay Can on 6/14/23.
 //
-
-import Foundation
-import Flutter
-import KontaktSDK
-
-
-// This scan service relies on Core Bluetooth and uses Kontakt Secure packages.
-
-class SecureScanService: NSObject,FlutterStreamHandler,KTKDevicesManagerDelegate {
-    
-//    private var secureProfileDiscoveredEventSink : FlutterEventSink? = nil
-    private var secureProfilesUpdatedEventSink : FlutterEventSink? = nil
-//    private var secureProfileLostEvenSink : FlutterEventSink? = nil
-    
-    
-    
-    
-    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        
-    }
-    
-    func onCancel(withArguments arguments: Any?) -> FlutterError? {
-      
-    }
-    
-    func devicesManager(_ manager: KTKDevicesManager, didDiscover devices: [KTKNearbyDevice]) {
-       
-    }
-    
-    
-}
+//// This scan service relies on Core Bluetooth and uses Kontakt Secure packages.
+//
+//class SecureScanService: NSObject,FlutterStreamHandler,KTKDevicesManagerDelegate {
+//
+////    private var secureProfileDiscoveredEventSink : FlutterEventSink? = nil
+//    private var secureProfilesUpdatedEventSink : FlutterEventSink? = nil
+////    private var secureProfileLostEvenSink : FlutterEventSink? = nil
+//
+//
+//
+//
+//    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+//
+//    }
+//
+//    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+//
+//    }
+//
+//    func devicesManager(_ manager: KTKDevicesManager, didDiscover devices: [KTKNearbyDevice]) {
+//
+//    }
+//
+//
+//}
