2681caa400c413e63565b883a67f1d0e5ab1a2a1
devberkay <canbr29@gmail.com>
2023-06-30 20:35:13 +0000

    Jun 30, 2023, 11:35 PM

--- ios/Classes/SecureScanService.swift
+++ ios/Classes/SecureScanService.swift
@@ -12,27 +12,27 @@ import KontaktSDK
 
 // This scan service relies on Core Bluetooth and uses Kontakt Secure packages.
 
-//class SecureScanService: NSObject,FlutterStreamHandler,KTKDevicesManagerDelegate {
-//    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-//        <#code#>
-//    }
-//    
-//    func onCancel(withArguments arguments: Any?) -> FlutterError? {
-//        <#code#>
-//    }
-//    
-//    func devicesManager(_ manager: KTKDevicesManager, didDiscover devices: [KTKNearbyDevice]) {
-//        <#code#>
-//    }
-//    
-//
-////    private var secureProfileDiscoveredEventSink : FlutterEventSink? = nil
-//    private var KTKDevicesUpdatedEventSink : FlutterEventSink? = nil
-////    private var secureProfileLostEvenSink : FlutterEventSink? = nil
-//
-//    
-//
-//
-//
-//
-//}
+class SecureScanService: NSObject,FlutterStreamHandler,KTKDevicesManagerDelegate {
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        <#code#>
+    }
+
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        <#code#>
+    }
+
+    func devicesManager(_ manager: KTKDevicesManager, didDiscover devices: [KTKNearbyDevice]) {
+        <#code#>
+    }
+
+
+//    private var secureProfileDiscoveredEventSink : FlutterEventSink? = nil
+    private var KTKDevicesUpdatedEventSink : FlutterEventSink? = nil
+//    private var secureProfileLostEvenSink : FlutterEventSink? = nil
+
+
+
+
+
+
+}
