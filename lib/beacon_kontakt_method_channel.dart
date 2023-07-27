d713865ba438f9a0d11cb278d40347b49a146cd1
devberkay <canbr29@gmail.com>
2023-07-27 23:09:00 +0300

    v0.1.1

--- lib/beacon_kontakt_method_channel.dart
+++ lib/beacon_kontakt_method_channel.dart
@@ -174,7 +174,7 @@ class MethodChannelBeaconKontakt extends BeaconKontaktPlatform {
     while (true) {
       yield await methodChannel.invokeMethod<bool>("emitBluetoothStatus") ??
           false;
-      await Future.delayed(const Duration(seconds: 5));
+      await Future.delayed(const Duration(seconds: 8));
     }
   }
 
