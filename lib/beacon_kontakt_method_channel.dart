b47887da6896788e5648635652dbd5960f094040
devberkay <canbr29@gmail.com>
2023-08-16 23:47:54 +0300

    fix

--- lib/beacon_kontakt_method_channel.dart
+++ lib/beacon_kontakt_method_channel.dart
@@ -105,13 +105,13 @@ class MethodChannelBeaconKontakt extends BeaconKontaktPlatform {
             .map((e) => jsonDecode(jsonEncode(e)) as Map<String, dynamic>)
             .toList();
 
-        debugPrint("updatedInside : $listOfIBeaconsAsMap");
+        debugPrint("listenIBeaconsUpdated Error : $listOfIBeaconsAsMap");
         yield listOfIBeaconsAsMap
             .map((e) => IBeaconDevice.fromJson(e))
             .toList();
       }
     } on PlatformException catch (e) {
-      // debugPrint("listenIBeaconsUpdated Error : ${e.message}");
+      debugPrint("listenIBeaconsUpdated Error : ${e.message}");
     }
   }
 
